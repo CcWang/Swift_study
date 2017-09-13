4b36556b44f80985538103aea84388e47aea3b37
CC Wang <chongchong1110@gmail.com>
2017-09-13 14:25:14 -0700

    UIbutton

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    mark properties:
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+//mark: actions
+    
 
+    @IBAction func changeLabelValue(_ sender: UIButton) {
+        nameLabel.text = "Live a better day!";
+
+    }
+    
+    
 }
 
